07b4e659392807e787f7be6ad85c22eb38dd940c
tl86br <tl86br@gmail.com>
2018-10-07 12:13:00 -0300

    Adds handle set cookie for access keys; Adjusts tags attributes

--- 00 Extra/01-ajax-xhr/app.js
+++ 00 Extra/01-ajax-xhr/app.js
@@ -1,17 +1,40 @@
 const requestUnsplash = new XMLHttpRequest();
 const searchForText = getElementBy('#search-text').value || 'sea'
 
+// const apiCredentials = [
+  // {key: 'unsplash'},
+  // {key: 'nytimes'},
+// ]
+
+function setTimeValueBy(time) {
+  return {'1-minute': 60, '10-minutes': 60 * 10, 'one-hour': 60 * 60, 'one-day': 60 * 60 * 24 }[time]
+}
+
+function getCookieExpiredTime(time='one-day') {
+  const now = new Date()
+  now.setTime(now.getTime() + 1 * setTimeValueBy(time) * 1000 )
+  return now.toGMTString()
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  const target = getElementBy('#access-key')
-  const storedKey = localStorage.getItem('unsplash')
-  console.dir(target)
-  console.log(storedKey)
-  target.value = storedKey
-  console.log(target)
+  const targets = getEachElementsWith('[data-event="set-keys"]')
+  // apiCredentials.forEach(({key}) => console.log(key))
+
+  targets.forEach(target => {
+    const key = target.dataset.target
+    const input = getElementBy(`[data-input="${key}"]`)
+    if (localStorage.getItem(key)) input.value = localStorage.getItem(key)
+  })
+
+  targets.forEach(target => target.addEventListener('click', function() {
+    const key = target.dataset.target
+    const input = getElementBy(`[data-input="${key}"]`)
+    const accessKey = input.value
+    console.log(input);
+    localStorage.setItem(key, accessKey)
+  }))
 
 })
-
-console.log(searchForText);
 
 function showResponse(event) {
   console.log(event)
@@ -21,24 +44,26 @@ function getElementBy(targetReference) {
   return document.querySelector(targetReference)
 }
 
+function getEachElementsWith(targetsReference) {
+  return Array.from(document.querySelectorAll(targetsReference))
+}
+
 function getAccessKeyOf(apiName) {
   return {
-    'unsplash': getElementBy('#access-key').value || localStorage.getItem('unsplash')
+    'unsplash': handleStorageOfAccessKey('unsplash'),
+    'nytimes': handleStorageOfAccessKey('nytimes')
   }[apiName]
 }
 
-function setKeys() {
-  const accessKey = getAccessKeyOf('unsplash')
-  console.log('hey');
-  localStorage.setItem('unsplash', accessKey)
+function handleStorageOfAccessKey(apiName) {
+  return localStorage.getItem(apiName) || getElementBy(`[data-input="${apiName}"]`).value
 }
+
 
 requestUnsplash.open('GET', `https://api.unsplash.com/search/photos?page=1&query=city`)
 requestUnsplash.onload = showResponse
 const accessKey = getAccessKeyOf('unsplash')
-console.log('accessKey', accessKey)
+console.log(getAccessKeyOf('unsplash'))
+console.log(getAccessKeyOf('nytimes'))
 requestUnsplash.setRequestHeader('Authorization', `Client-ID ${accessKey}`)
-requestUnsplash.send()
-// console.log();
-const button = document.querySelector('#set-keys')
-button.onclick = setKeys
+// requestUnsplash.send()
