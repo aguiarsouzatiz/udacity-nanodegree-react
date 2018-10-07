e1fd107013d492f6fcaa4a4100c83a8b066cb025
tl86br <tl86br@gmail.com>
2018-10-07 17:14:21 -0300

    Replaces storage from localStorage to cookie with expire date

--- 00 Extra/01-ajax-xhr/app.js
+++ 00 Extra/01-ajax-xhr/app.js
@@ -1,41 +1,48 @@
-const requestUnsplash = new XMLHttpRequest();
-const searchForText = getElementBy('#search-text').value || 'sea'
+
 
 // const apiCredentials = [
   // {key: 'unsplash'},
   // {key: 'nytimes'},
 // ]
 
-function setTimeValueBy(time) {
+function setTimeValueBy(time='one-day') {
   return {'1-minute': 60, '10-minutes': 60 * 10, 'one-hour': 60 * 60, 'one-day': 60 * 60 * 24 }[time]
 }
 
-function getCookieExpiredTime(time='one-day') {
+function getCookieExpiredTime(time) {
   const now = new Date()
   now.setTime(now.getTime() + 1 * setTimeValueBy(time) * 1000 )
   return now.toGMTString()
 }
 
+function getCookieWith(key) {
+  const cookies = document.cookie.split(';')
+  return cookies.find(cookie => cookie.includes(key)).trim()
+}
+
+function extactContentOf(cookie) {
+   const [ key, value ] = cookie.split('\=')
+   return { key, value }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const targets = getEachElementsWith('[data-event="set-keys"]')
-  // apiCredentials.forEach(({key}) => console.log(key))
 
   targets.forEach(target => {
     const key = target.dataset.target
     const input = getElementBy(`[data-input="${key}"]`)
-    // console.log(input);
+    const targetCookie = extactContentOf(getCookieWith(key))
 
-    if (localStorage.getItem(key)) input.value = localStorage.getItem(key)
+    if (targetCookie) input.value = targetCookie.value
   })
 
   targets.forEach(target => target.addEventListener('click', function() {
     const key = target.dataset.target
     const input = getElementBy(`[data-input="${key}"]`)
     const accessKey = input.value
-    console.log(input)
-    localStorage.setItem(key, accessKey)
-  }))
 
+    document.cookie = `${key}=${accessKey};expires=${getCookieExpiredTime("one-day")};`
+  }))
 })
 
 function showResponse(event) {
@@ -58,13 +65,14 @@ function getAccessKeyOf(apiName) {
 }
 
 function handleStorageOfAccessKey(apiName) {
-  return localStorage.getItem(apiName) || getElementBy(`[data-input="${apiName}"]`).value
+  return extactContentOf(getCookieWith(apiName)).value || getElementBy(`[data-input="${apiName}"]`).value
 }
+
+const requestUnsplash = new XMLHttpRequest();
+const searchForText = getElementBy('#search-text').value || ''
 
 requestUnsplash.open('GET', `https://api.unsplash.com/search/photos?page=1&query=city`)
 requestUnsplash.onload = showResponse
 const accessKey = getAccessKeyOf('unsplash')
-console.log(getAccessKeyOf('unsplash'))
-console.log(getAccessKeyOf('nytimes'))
 requestUnsplash.setRequestHeader('Authorization', `Client-ID ${accessKey}`)
 // requestUnsplash.send()
--- 00 Extra/01-ajax-xhr/assets/transpiled.js
+++ 00 Extra/01-ajax-xhr/assets/transpiled.js
@@ -1,35 +1,49 @@
 'use strict';
 
-var requestUnsplash = new XMLHttpRequest();
-var searchForText = getElementBy('#search-text').value || 'sea';
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 // const apiCredentials = [
 // {key: 'unsplash'},
 // {key: 'nytimes'},
 // ]
 
-function setTimeValueBy(time) {
+function setTimeValueBy() {
+  var time = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'one-day';
+
   return { '1-minute': 60, '10-minutes': 60 * 10, 'one-hour': 60 * 60, 'one-day': 60 * 60 * 24 }[time];
 }
 
-function getCookieExpiredTime() {
-  var time = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'one-day';
-
+function getCookieExpiredTime(time) {
   var now = new Date();
   now.setTime(now.getTime() + 1 * setTimeValueBy(time) * 1000);
   return now.toGMTString();
 }
 
+function getCookieWith(key) {
+  var cookies = document.cookie.split(';');
+  return cookies.find(function (cookie) {
+    return cookie.includes(key);
+  }).trim();
+}
+
+function extactContentOf(cookie) {
+  var _cookie$split = cookie.split('\='),
+      _cookie$split2 = _slicedToArray(_cookie$split, 2),
+      key = _cookie$split2[0],
+      value = _cookie$split2[1];
+
+  return { key: key, value: value };
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   var targets = getEachElementsWith('[data-event="set-keys"]');
-  // apiCredentials.forEach(({key}) => console.log(key))
 
   targets.forEach(function (target) {
     var key = target.dataset.target;
     var input = getElementBy('[data-input="' + key + '"]');
-    // console.log(input);
+    var targetCookie = extactContentOf(getCookieWith(key));
 
-    if (localStorage.getItem(key)) input.value = localStorage.getItem(key);
+    if (targetCookie) input.value = targetCookie.value;
   });
 
   targets.forEach(function (target) {
@@ -37,8 +51,8 @@ document.addEventListener('DOMContentLoaded', function () {
       var key = target.dataset.target;
       var input = getElementBy('[data-input="' + key + '"]');
       var accessKey = input.value;
-      // console.log(input)
-      localStorage.setItem(key, accessKey);
+
+      document.cookie = key + '=' + accessKey + ';expires=' + getCookieExpiredTime("one-day") + ';';
     });
   });
 });
@@ -63,13 +77,14 @@ function getAccessKeyOf(apiName) {
 }
 
 function handleStorageOfAccessKey(apiName) {
-  return localStorage.getItem(apiName) || getElementBy('[data-input="' + apiName + '"]').value;
+  return extactContentOf(getCookieWith(apiName)).value || getElementBy('[data-input="' + apiName + '"]').value;
 }
+
+var requestUnsplash = new XMLHttpRequest();
+var searchForText = getElementBy('#search-text').value || '';
 
 requestUnsplash.open('GET', 'https://api.unsplash.com/search/photos?page=1&query=city');
 requestUnsplash.onload = showResponse;
 var accessKey = getAccessKeyOf('unsplash');
-console.log(getAccessKeyOf('unsplash'));
-console.log(getAccessKeyOf('nytimes'));
 requestUnsplash.setRequestHeader('Authorization', 'Client-ID ' + accessKey);
 // requestUnsplash.send()
